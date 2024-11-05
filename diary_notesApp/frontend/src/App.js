1e5371ac55b2bb186a903aa5fb5d6ec88ba5c049
Hesham Tarek <tarekhesham184@gmail.com>
2024-11-05 15:55:56 +0200

    Update files

---
+++ diary_notesApp/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    /* JSX starts here */
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+    /* JSX ends here */
+  );
+}
+
+export default App;
